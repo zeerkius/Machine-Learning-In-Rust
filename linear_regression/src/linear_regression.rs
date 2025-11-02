a9e77abdc709e9a8f3f20ccca6d1e84f26245157
zeerkius <90996736+zeerkius@users.noreply.github.com>
2025-11-02 00:06:03 -0500

    Update sse_gradient to use input feature xi
    Modified sse_gradient function to include xi parameter for gradient calculation.

--- linear_regression/src/linear_regression.rs
+++ linear_regression/src/linear_regression.rs
@@ -1,9 +1,3 @@
-
-    
-
-
-    
-
 // This is a rust implementation of a Linear Regression Model
 // We need a struct to Define Training / Testing Data
 use std::collections::HashSet;
@@ -34,9 +28,9 @@ impl LRModel{
             Ok(dot_prod) // preferred return type
         }
     }
-    fn sse_gradient(&self,prediction : f64 , ground_truth : f64) -> f64{
+    fn sse_gradient(&self,prediction : f64 , ground_truth : f64,xi:f64) -> f64{
         // we purposely omit the constant
-        let delta : f64 = (prediction - ground_truth);
+        let delta : f64 = (prediction - ground_truth) * xi;
         delta
     }
     
@@ -82,7 +76,7 @@ impl LRModel{
                 }else{
                     let mut yi : f64 = self.dot_product(&weight_vector,&X[j]).unwrap(); // point to values to avoid moving errors
                     for n in 0..length_input_vector{
-                        error_cache[n].push(self.sse_gradient(yi,Y[j]))
+                        error_cache[n].push(self.sse_gradient(yi,Y[j],X[j][n]));
                     }
                 }
             }
